87a4c2a23d383ce17b7a79f887ee64bee2a1fadc
Dounia Rtaili <drtaili@e1r8p15.1337.ma>
2023-12-18 20:44:16 +0100

    add: main

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: drtaili <drtaili@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 17:39:27 by drtaili           #+#    #+#             */
-/*   Updated: 2023/12/16 20:50:54 by drtaili          ###   ########.fr       */
+/*   Updated: 2023/12/17 22:50:32 by drtaili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,21 @@
 
 #include <iostream>
 #include <stack>
-#include <vector>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    // Iterator typedefs
+    // the typedef lines are creating aliases iterator and const_iterator for the iterator 
+    // and const_iterator types of the underlying container, making it easier to use them in the rest of your MutantStack class.
+    // The container_type is an alias provided by std::stack for its underlying container type. In this case, 
+    // it refers to the type of the container used to implement the stack, which defaults to std::deque<T>.
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-    // Constructor
     MutantStack() : std::stack<T>() {}
-
-    // Destructor
     ~MutantStack() {}
-
-    // Copy constructor
     MutantStack(const MutantStack& other) : std::stack<T>(other) {}
-
-    // Copy assignment operator
     const MutantStack& operator=(const MutantStack& other)
     {
         if (this != &other)
@@ -44,9 +39,10 @@ public:
         return *this;
     }
 
-    // Begin and end functions to access iterators
     iterator begin() { return this->c.begin(); } 
     iterator end() { return this->c.end(); }
+    // The const_iterator is used when the object is const-qualified, 
+    // ensuring that you cannot modify the elements through that iterator.
     const_iterator begin() const { return this->c.begin(); }
     const_iterator end() const { return this->c.end(); }
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,43 +6,64 @@
 /*   By: drtaili <drtaili@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 17:39:32 by drtaili           #+#    #+#             */
-/*   Updated: 2023/12/16 20:42:06 by drtaili          ###   ########.fr       */
+/*   Updated: 2023/12/18 19:36:47 by drtaili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
+class Employee{
+public:
+    int days;
+    Employee(int days){
+        std::cout << "constructor called" << days << std::endl;
+    }
+    Employee(const Employee &employee){
+        std::cout << "copy constructor called" << employee.days << std::endl;
+        this->days = employee.days;
+    }
+};
+
 int main()
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    std::cout << "-----------------------" << std::endl;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    std::cout << "-----------------------" << std::endl;
-    std::cout << mstack.size() << std::endl;
-    std::stack<int> s(mstack);
-    s.push(5);
-    s.push(17);
-    std::cout << s.top() << std::endl;
-    s.pop();
-    std::cout << s.size() << std::endl;
+    // MutantStack<int> mstack;
+    // mstack.push(5);
+    // mstack.push(17);
+    // std::cout << mstack.top() << std::endl;
+    // mstack.pop();
+    // std::cout << "mstack size : " << mstack.size() << std::endl;
+    // mstack.push(3);
+    // mstack.push(5);
+    // mstack.push(0);
+    // MutantStack<int>::iterator it = mstack.begin();
+    // MutantStack<int>::iterator ite = mstack.end();
+    // ++it;
+    // --it;
+    // std::cout << "-----------------------" << std::endl;
+    // while (it != ite)
+    // {
+    //     std::cout << *it << std::endl;
+    //     ++it;
+    // }
+    // std::cout << "-----------------------" << std::endl;
+    // std::stack<int> s(mstack);
+    // std::cout << "s size : " << s.size() << std::endl;
+    // std::cout << "Elements of the stack: "<< std::endl;
+    // while (!s.empty()) {
+    //     std::cout << s.top() << std::endl;
+    //     s.pop();
+    // }
+    // std::cout << "s size : " << s.size() << std::endl;
+
+    // std::stack <Employee, std::deque<Employee> > employee_stack;
+    // Employee joe(10);
+    // employee_stack.push(joe);
+    // employee_stack.emplace(joe); //the object was being created and emplaced at the same time
+    // employee_stack.push_back(6);
+
+    std::stack<int, std::deque<int> > st;
+    st.push_back(20);
     
     return 0;
 }
+
